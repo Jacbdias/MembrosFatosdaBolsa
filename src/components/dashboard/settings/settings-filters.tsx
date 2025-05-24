9f88ac77f018b2650716e56c5bf09839ca41c255
Jacbdias <fatosdabolsa@gmail.com>
2025-05-24 19:08:38 -0300

    Update settings-filters.tsx

--- src/components/dashboard/settings/settings-filters.tsx
+++ src/components/dashboard/settings/settings-filters.tsx
@@ -1,23 +1,9 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import InputAdornment from '@mui/material/InputAdornment';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import { MagnifyingGlass as MagnifyingGlassIcon } from '@phosphor-icons/react/dist/ssr/MagnifyingGlass';
 
 export function SettingsFilters(): React.JSX.Element {
   return (
-    <Card sx={{ p: 2 }}>
-      <OutlinedInput
-        defaultValue=""
-        fullWidth
-        placeholder="Buscar ações..."
-        startAdornment={
-          <InputAdornment position="start">
-            <MagnifyingGlassIcon fontSize="var(--icon-fontSize-md)" />
-          </InputAdornment>
-        }
-        sx={{ maxWidth: '500px' }}
-      />
-    </Card>
+    <div>
+      <h3>Filtros de Settings funcionando!</h3>
+    </div>
   );
 }
