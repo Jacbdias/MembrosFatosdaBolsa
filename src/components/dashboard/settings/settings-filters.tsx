cff32447f22b539f3036e1245ae29c1a1dee0c63
Jacbdias <fatosdabolsa@gmail.com>
2025-05-24 20:33:40 -0300

    Update settings-filters.tsx

--- src/components/dashboard/settings/settings-filters.tsx
+++ src/components/dashboard/settings/settings-filters.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
 
-export function SettingsFilters(): React.JSX.Element {
-  return (
-    <div>
-      <h3>Filtros de Settings funcionando!</h3>
-    </div>
-  );
+export function IntegrationsFilters(): React.JSX.Element {
+  return null;
 }
