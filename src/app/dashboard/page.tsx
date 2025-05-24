dfa091a6f06e1cac8b01dab145017226fc31e029
Jacbdias <fatosdabolsa@gmail.com>
2025-05-24 14:34:14 -0300

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,5 @@
-import * as React from 'react';
-import type { Metadata } from 'next';
-import Grid from '@mui/material/Unstable_Grid2';
+import { redirect } from 'next/navigation';
 
-import { config } from '@/config';
-import { OverviewFilters } from '@/components/dashboard/overview/overview-filters';
-import { OverviewTable } from '@/components/dashboard/overview/overview-table';
-
-export const metadata = { title: `Overview | Dashboard | ${config.site.name}` } satisfies Metadata;
-
-export default function Page(): React.JSX.Element {
-  return (
-    <Grid container spacing={3}>
-      <Grid xs={12}>
-        <OverviewFilters />
-      </Grid>
-      <Grid xs={12}>
-        <OverviewTable />
-      </Grid>
-    </Grid>
-  );
+export default function DashboardPage() {
+  redirect('/dashboard/overview');
 }
