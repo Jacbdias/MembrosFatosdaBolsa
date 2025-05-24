3b365d8739b4ad733be0a8047da6868f02435208
Jacbdias <fatosdabolsa@gmail.com>
2025-05-24 09:08:03 -0300

    Update nav-icons.tsx

--- src/components/dashboard/layout/nav-icons.tsx
+++ src/components/dashboard/layout/nav-icons.tsx
@@ -1,6 +1,7 @@
 import type { Icon } from '@phosphor-icons/react/dist/lib/types';
 import { ChartPie as ChartPieIcon } from '@phosphor-icons/react/dist/ssr/ChartPie';
-import { ChartBar } from '@phosphor-icons/react/dist/ssr/ChartBar';
+import { ChartBar as ChartBarIcon } from '@phosphor-icons/react/dist/ssr/ChartBar';
+import { ChartLineUp } from '@phosphor-icons/react/dist/ssr/ChartLineUp';
 import { GearSix as GearSixIcon } from '@phosphor-icons/react/dist/ssr/GearSix';
 import { PlugsConnected as PlugsConnectedIcon } from '@phosphor-icons/react/dist/ssr/PlugsConnected';
 import { User as UserIcon } from '@phosphor-icons/react/dist/ssr/User';
@@ -9,7 +10,8 @@ import { XSquare } from '@phosphor-icons/react/dist/ssr/XSquare';
 
 export const navIcons = {
   'chart-pie': ChartPieIcon,
-  'chart-bar': ChartBar,           // novo ícone para Micro Caps
+  'chart-bar': ChartBarIcon,
+  'chart-line-up': ChartLineUp,
   'gear-six': GearSixIcon,
   'plugs-connected': PlugsConnectedIcon,
   'x-square': XSquare,
