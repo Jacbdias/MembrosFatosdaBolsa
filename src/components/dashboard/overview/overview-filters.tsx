a4722da468b1c054c06b574343123e9287ed297a
Jacbdias <fatosdabolsa@gmail.com>
2025-05-24 15:28:09 -0300

    Update overview-filters.tsx

--- src/components/dashboard/overview/overview-filters.tsx
+++ src/components/dashboard/overview/overview-filters.tsx
@@ -1,23 +1,5 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import InputAdornment from '@mui/material/InputAdornment';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import { MagnifyingGlass as MagnifyingGlassIcon } from '@phosphor-icons/react/dist/ssr/MagnifyingGlass';
 
 export function OverviewFilters(): React.JSX.Element {
-  return (
-    <Card sx={{ p: 2 }}>
-      <OutlinedInput
-        defaultValue=""
-        fullWidth
-        placeholder="Search overview data"
-        startAdornment={
-          <InputAdornment position="start">
-            <MagnifyingGlassIcon fontSize="var(--icon-fontSize-md)" />
-          </InputAdornment>
-        }
-        sx={{ maxWidth: '500px' }}
-      />
-    </Card>
-  );
+  return null;
 }
