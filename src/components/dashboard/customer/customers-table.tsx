801e77e8383c029bfde9cd0a9ad543b70b4af9e3
Jacbdias <fatosdabolsa@gmail.com>
2025-05-24 09:16:54 -0300

    Update customers-table.tsx

--- src/components/dashboard/customer/customers-table.tsx
+++ src/components/dashboard/customer/customers-table.tsx
@@ -14,43 +14,39 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
-import dayjs from 'dayjs';
 
 import { useSelection } from '@/hooks/use-selection';
 
-function noop(): void {
-  // do nothing
-}
+function noop(): void {}
 
-export interface Customer {
+export interface Ativo {
   id: string;
   avatar: string;
-  name: string;
-  email: string;
-  address: { city: string; state: string; country: string; street: string };
-  phone: string;
-  createdAt: Date;
+  ticker: string;
+  setor: string;
+  dataEntrada: string;
+  precoEntrada: string;
+  precoAtual: string;
+  dy: string;
+  precoTeto: string;
 }
 
-interface CustomersTableProps {
+interface AtivosTableProps {
   count?: number;
   page?: number;
-  rows?: Customer[];
+  rows?: Ativo[];
   rowsPerPage?: number;
 }
 
-export function CustomersTable({
+export function AtivosTable({
   count = 0,
   rows = [],
   page = 0,
   rowsPerPage = 0,
-}: CustomersTableProps): React.JSX.Element {
-  const rowIds = React.useMemo(() => {
-    return rows.map((customer) => customer.id);
-  }, [rows]);
+}: AtivosTableProps): React.JSX.Element {
+  const rowIds = React.useMemo(() => rows.map((ativo) => ativo.id), [rows]);
 
   const { selectAll, deselectAll, selectOne, deselectOne, selected } = useSelection(rowIds);
-
   const selectedSome = (selected?.size ?? 0) > 0 && (selected?.size ?? 0) < rows.length;
   const selectedAll = rows.length > 0 && selected?.size === rows.length;
 
@@ -65,51 +61,46 @@ export function CustomersTable({
                   checked={selectedAll}
                   indeterminate={selectedSome}
                   onChange={(event) => {
-                    if (event.target.checked) {
-                      selectAll();
-                    } else {
-                      deselectAll();
-                    }
+                    if (event.target.checked) selectAll();
+                    else deselectAll();
                   }}
                 />
               </TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Location</TableCell>
-              <TableCell>Phone</TableCell>
-              <TableCell>Signed Up</TableCell>
+              <TableCell>Ativo</TableCell>
+              <TableCell>Setor</TableCell>
+              <TableCell>Data de Entrada</TableCell>
+              <TableCell>Preço que Iniciou</TableCell>
+              <TableCell>Preço Atual</TableCell>
+              <TableCell>DY</TableCell>
+              <TableCell>Preço Teto</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => {
               const isSelected = selected?.has(row.id);
-
               return (
                 <TableRow hover key={row.id} selected={isSelected}>
                   <TableCell padding="checkbox">
                     <Checkbox
                       checked={isSelected}
                       onChange={(event) => {
-                        if (event.target.checked) {
-                          selectOne(row.id);
-                        } else {
-                          deselectOne(row.id);
-                        }
+                        if (event.target.checked) selectOne(row.id);
+                        else deselectOne(row.id);
                       }}
                     />
                   </TableCell>
                   <TableCell>
-                    <Stack sx={{ alignItems: 'center' }} direction="row" spacing={2}>
+                    <Stack direction="row" spacing={2} alignItems="center">
                       <Avatar src={row.avatar} />
-                      <Typography variant="subtitle2">{row.name}</Typography>
+                      <Typography variant="subtitle2">{row.ticker}</Typography>
                     </Stack>
                   </TableCell>
-                  <TableCell>{row.email}</TableCell>
-                  <TableCell>
-                    {row.address.city}, {row.address.state}, {row.address.country}
-                  </TableCell>
-                  <TableCell>{row.phone}</TableCell>
-                  <TableCell>{dayjs(row.createdAt).format('MMM D, YYYY')}</TableCell>
+                  <TableCell>{row.setor}</TableCell>
+                  <TableCell>{row.dataEntrada}</TableCell>
+                  <TableCell>{row.precoEntrada}</TableCell>
+                  <TableCell>{row.precoAtual}</TableCell>
+                  <TableCell>{row.dy}</TableCell>
+                  <TableCell>{row.precoTeto}</TableCell>
                 </TableRow>
               );
             })}
