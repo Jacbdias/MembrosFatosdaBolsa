212e9ef9054cf96b2377e493ddeb8ad9811d37ff
Jacbdias <fatosdabolsa@gmail.com>
2025-05-25 17:53:03 -0300

    Update config.ts

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -6,6 +6,6 @@ export const navItems = [
   { key: 'customers', title: 'Micro Caps', href: paths.dashboard.customers, icon: 'chart-bar' },
   { key: 'integrations', title: 'Dividendos', href: paths.dashboard.integrations, icon: 'chart-line-up' },
   { key: 'settings', title: 'Fundos Imobiliários', href: paths.dashboard.settings, icon: 'buildings' },
-  { key: 'internacional', title: 'Internacional', href: '/dashboard/internacional', icon: 'globe' },
+  // { key: 'internacional', title: 'Internacional', href: '/dashboard/internacional', icon: 'globe' },
   { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
 ] satisfies NavItemConfig[];
