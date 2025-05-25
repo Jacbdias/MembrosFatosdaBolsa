58f51fa8164f07ddfaa6b5de8175c56401e629bc
Jacbdias <fatosdabolsa@gmail.com>
2025-05-25 09:12:25 -0300

    Create page.tsx

---
+++ src/app/dashboard/exterior/page.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import type { Metadata } from 'next';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { Download as DownloadIcon } from '@phosphor-icons/react/dist/ssr/Download';
+import { Plus as PlusIcon } from '@phosphor-icons/react/dist/ssr/Plus';
+import { Upload as UploadIcon } from '@phosphor-icons/react/dist/ssr/Upload';
+
+import { config } from '@/config';
+import { ExteriorFilters } from '@/components/dashboard/exterior/exterior-filters';
+import { ExteriorTable } from '@/components/dashboard/exterior/exterior-table';
+import { ExteriorSelectionProvider } from '@/components/dashboard/exterior/exterior-selection-provider';
+
+export const metadata: Metadata = {
+  title: `Exterior | Dashboard | ${config.site.name}`,
+};
+
+export default function Page(): React.JSX.Element {
+  return (
+    <ExteriorSelectionProvider>
+      <Stack spacing={3}>
+        <Stack direction="row" spacing={3}>
+          <Stack spacing={1} sx={{ flex: '1 1 auto' }}>
+            <Typography variant="h4">Investimentos Exterior</Typography>
+            <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
+              <Button color="inherit" startIcon={<UploadIcon fontSize="var(--icon-fontSize-md)" />}>
+                Importar
+              </Button>
+              <Button color="inherit" startIcon={<DownloadIcon fontSize="var(--icon-fontSize-md)" />}>
+                Exportar
+              </Button>
+            </Stack>
+          </Stack>
+          <div>
+            <Button startIcon={<PlusIcon fontSize="var(--icon-fontSize-md)" />} variant="contained">
+              Adicionar
+            </Button>
+          </div>
+        </Stack>
+        <ExteriorFilters />
+        <ExteriorTable />
+      </Stack>
+    </ExteriorSelectionProvider>
+  );
+}
