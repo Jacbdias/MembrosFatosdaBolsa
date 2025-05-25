5b7c1e27334126a65197e49ed53f802d58dfb6d9
Jacbdias <fatosdabolsa@gmail.com>
2025-05-25 17:26:32 -0300

    Update config.ts

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -1,11 +1,8 @@
-import type { NavItemConfig } from '@/types/nav';
-import { paths } from '@/paths';
-
 export const navItems = [
   { key: 'overview', title: 'Small Caps', href: '/dashboard/overview', icon: 'chart-pie' },
   { key: 'customers', title: 'Micro Caps', href: paths.dashboard.customers, icon: 'chart-bar' },
   { key: 'integrations', title: 'Dividendos', href: paths.dashboard.integrations, icon: 'chart-line-up' },
   { key: 'settings', title: 'Fundos Imobiliários', href: paths.dashboard.settings, icon: 'buildings' },
+  { key: 'internacional', title: 'Internacional', href: paths.dashboard.internacional, icon: 'globe' }, // ← ADICIONAR ESTA LINHA
   { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
-  { key: 'internacional', title: 'Internacional', href: paths.dashboard.internacional, icon: 'globe' },
 ] satisfies NavItemConfig[];
