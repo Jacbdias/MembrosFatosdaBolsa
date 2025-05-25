5b5237eadc0b3db1d540918932ed5d65bbac91ac
Jacbdias <fatosdabolsa@gmail.com>
2025-05-25 17:56:21 -0300

    Update paths.ts

--- src/paths.ts
+++ src/paths.ts
@@ -1,13 +1,13 @@
 export const paths = {
   home: '/',
   auth: { signIn: '/auth/sign-in', signUp: '/auth/sign-up', resetPassword: '/auth/reset-password' },
-dashboard: {
-  overview: '/dashboard/overview',
-  account: '/dashboard/account',
-  customers: '/dashboard/customers',
-  internacional: '/dashboard/internacional', // ← ESTA LINHA
-  integrations: '/dashboard/integrations',
-  settings: '/dashboard/settings',
-},
+  dashboard: {
+    overview: '/dashboard/overview',
+    account: '/dashboard/account',
+    customers: '/dashboard/customers',
+    internacional: '/dashboard/internacional',
+    integrations: '/dashboard/integrations',
+    settings: '/dashboard/settings',
+  },
   errors: { notFound: '/errors/not-found' },
 } as const;
