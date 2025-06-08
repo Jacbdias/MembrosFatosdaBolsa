0536edd90078d232143186d1dcebad8ff2293c6e
Jacbdias <fatosdabolsa@gmail.com>
2025-06-08 17:43:56 -0300

    Create page.tsx

---
+++ src/admin/page.tsx
@@ -0,0 +1,10 @@
+import AdminIntegrado from '@/components/AdminIntegrado';
+
+export default function AdminPage() {
+  return <AdminIntegrado />;
+}
+
+export const metadata = {
+  title: 'Administração do Portfólio',
+  description: 'Painel de administração para gerenciar ativos, proventos e relatórios'
+};
